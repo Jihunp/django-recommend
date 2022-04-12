3a4b5f4fdcec4205f04a44538401be1149a085a1
Jihunp <edwardpark.park@gmail.com>
2022-04-11 23:02:26 -0700

    enlarged images

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -3,14 +3,17 @@ const triggerArray = Array.from(triggerBtn).entries();
 const modals = document.getElementsByClassName("modal");
 const closeBtn = document.getElementsByClassName("close");
 
+// for of loop that takes each instance of the button and toggles open and close for the modal
 for (let [i, trigger] of triggerArray) {
     toggleModals = function() {
+        // class named my-modal
         modals[i].classList.toggle("my-modal");
     };
     trigger.addEventListener("click", toggleModals);
     closeBtn[i].addEventListener("click", toggleModals);
 }
 // escape all modals with esc key
+// bug in use
 window.onkeydown = function(event) {
     if(event.key == 'Escape') {
         for(let index of modals) {
