b2c6f2a9b34a90e7cd1cf74336567b74b43ff886
Jihunp <edwardpark.park@gmail.com>
2022-04-12 11:51:53 -0700

    review modals created

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -7,7 +7,7 @@ const closeBtn = document.getElementsByClassName("close");
 for (let [i, trigger] of triggerArray) {
     toggleModals = function() {
         // class named my-modal
-        modals[i].classList.toggle("my-modal");
+        modals[i].classList.toggle("modal");
     };
     trigger.addEventListener("click", toggleModals);
     closeBtn[i].addEventListener("click", toggleModals);
@@ -18,6 +18,36 @@ window.onkeydown = function(event) {
     if(event.key == 'Escape') {
         for(let index of modals) {
             index.style.display = 'none'
+        }
+    }
+}
+
+// modal number two for review
+const btnTrigger = document.getElementsByClassName("trigger-2");
+const arrayTrigger = Array.from(btnTrigger).entries();
+const mods = document.getElementsByClassName("modal-2");
+const btnClose = document.getElementsByClassName("close-2");
+
+// for of loop that takes each instance of the button and toggles open and close for the modal
+for (let [index, trigun] of arrayTrigger) {
+    togglemods = function() {
+        // class named my-modal-2
+        mods[index].classList.toggle("modal-2");
+    };
+    trigun.addEventListener("click", togglemods);
+    btnClose[index].addEventListener("click", togglemods);
+}
+
+// escape all mods with esc key.
+// press open/close button once and then escape changes the way we view
+window.onkeydown = function(event) {
+    if(event.key == 'Escape') {
+        for(let i of mods) {
+            if (i.style.display === 'none') {
+                i.style.display = 'block';
+            } else {
+                i.style.display = 'none';
+            }
         }
     }
 }
