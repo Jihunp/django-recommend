e551b7097b90a615b14899b4a568bc9d38960e58
Jihunp <edwardpark.park@gmail.com>
2022-04-11 20:45:06 -0700

    optimized modal

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,93 +1,20 @@
-console.log("insanity check")
+const triggerBtn = document.getElementsByClassName("trigger");
+const triggerArray = Array.from(triggerBtn).entries();
+const modals = document.getElementsByClassName("modal");
+const closeBtn = document.getElementsByClassName("close");
 
-const modal = document.getElementsByClassName("modal");
-const btn = document.getElementsByClassName("modal-btn");
-// span element that closes the modal
-const span =  document.getElementsByClassName("close");
-
-[...btn].forEach((btn, i) => {
-    btn.onclick = () => (modal[i].style.display ='block');
-})
-
-
-// for (let modalBtn of btn) {
-//     modalBtn.onclick = function(event) {
-//         modalBtn = document.querySelector(event.target.getAttribute('href')).style.display ="block";
-//     }
-// }
-
-// for (let close of span) {
-//     close.onclick = function(event) {
-//         event.target.parentNode.parentNode.style.display = 'none';
-//     }
-// }
-// click outside of the modal exit modal
-window.onclick = function(event) {
-    if(event.target.classList.contains('modal')) {
-        for(let mod of modal) {
-            if(typeof mod.style !== 'undefined') {
-                mod.style.display = 'none'
-            }
-        }
-    }
+for (let [i, trigger] of triggerArray) {
+    toggleModals = function() {
+        modals[i].classList.toggle("my-modal");
+    };
+    trigger.addEventListener("click", toggleModals);
+    closeBtn[i].addEventListener("click", toggleModals);
 }
-// escape key exit modal
+// escape all modals with esc key
 window.onkeydown = function(event) {
     if(event.key == 'Escape') {
-        for(let mod of modal) {
-            mod.style.display = 'none'
+        for(let index of modals) {
+            index.style.display = 'none'
         }
     }
 }
-
-
-
-
-
-// user clicks button opens specific modal
-// for (let i = 0; i < btn.length; i++) {
-//     btn[i].onclick = function(e) {
-//         e.preventDefault();
-//         // we may have to add another name for each class
-//         // ele = document.getElementById("open-modal");
-//         // ele.classList.add("")
-//         mods = document.querySelector(e.target.getAttribute("href"));
-//         mods.style.display = "block";
-//     }
-// }
-
-// for (let i = 0; i < span.length; i++) {
-//     span[i].onclick = function() {
-//         for (let index in modal) {
-//             if (typeof modal[index].style !== 'undefined') modal[index].style.display = "none";
-//         }
-//     }
-// }
-
-// window.onclick = function(event) {
-//     if (event.target.classList.contains('modal')) {
-//         for(let index in modal) {
-//             if (typeof modal[index].style !== 'undefined') modal[index].style.display = "none";
-//         }
-//     }
-// }
-
-
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none"
-//     }
-// }
-
-
-
-
-
-
-
